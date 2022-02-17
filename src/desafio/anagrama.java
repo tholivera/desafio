873f2ebb24a226bc959c6107f977c5cbed463116
tholivera <emaildathaisoliveira.silva@gmail.com>
2022-02-17 17:04:05 -0300

    Atualizando exercício 3

--- src/desafio/anagrama.java
+++ src/desafio/anagrama.java
@@ -1,53 +1,38 @@
 package desafio;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class anagrama {
-public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+	    
+	    Scanner leia = new Scanner (System.in);
 		String palavra;
-		
-		Scanner leia = new Scanner(System.in);
-
-		System.out.println("Digite o texto : ");
-        palavra = leia.nextLine();
-		
-        anagrama(palavra);
-	}
-	
-	 static void anagrama( String palavra ){
-	        
-	        HashMap<String, Integer> mapa = new HashMap<>();
-
-	        for(int i  = 0; i <= palavra.length(); i++){
-	            
-	       
-	            for( int j = i; j < palavra.length(); j++ ){
-	                
-	                char[] caracter = palavra.substring(i, j+1).toCharArray();
-	                Arrays.sort(caracter);
-	                String subCaract = new String(caracter);
-
-	                if(mapa.containsKey(subCaract)){
-	                    mapa.put(subCaract, mapa.get(subCaract) + 1);
-	                }                    
-	                else{
-	                    mapa.put(subCaract,1);
-	                }                    
+	    
+	    System.out.println("Digite uma palavra");
+	    palavra = leia.nextLine();
+	    
+	    ArrayList<String> listaDeAnagramas = new ArrayList<String>();
+	    
+	    for(int i = 0; i <palavra.length(); i++) {
+	        for(int j = 1; j < palavra.length(); j++) {
+	            if(palavra.charAt(i) == palavra.charAt(j) && i !=j && i < j) {
+	                if(palavra.charAt(i) == palavra.charAt(i+1)) {
+	                String x = palavra.substring(i, i + 1);
+	                listaDeAnagramas.add(x);
+	                } else {
+	                String y = palavra.substring(i, j);
+	                String z = palavra.substring(i + 1, j + 1);
+	                String h = palavra.substring(i, i + 1);
+	                listaDeAnagramas.add(h);
+	                listaDeAnagramas.add(y);
+	                listaDeAnagramas.add(z);
+	                }
 	            }
 	        }
-
-	        int contaPares = 0;
-	        
-	        for(String cont: mapa.keySet()) {
-	            
-	            int n = mapa.get(cont);
-	            contaPares += (n*(n-1))/2;
-	        }
-	        System.out.print(contaPares);
 	    }
-
-
+	    
+	    System.out.println(listaDeAnagramas.size());
+	}
 }
