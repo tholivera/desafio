d30ff4fb639fe8e10c422f4724ff6439acd42f1a
tholivera <emaildathaisoliveira.silva@gmail.com>
2022-02-17 18:07:47 -0300

    Adicionando descrição do desafio 1

--- src/desafio/escada.java
+++ src/desafio/escada.java
@@ -1,3 +1,9 @@
+/* Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços. 
+ * A base e altura da escada devem ser iguais ao valor de n. 
+ * A última linha não deve conter nenhum espaço.
+ */
+
+
 package desafio;
 
 import java.util.ArrayList;
